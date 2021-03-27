3db300c226cc8588008958d02aad33568c841486
jyw19 <jyw19@ic.ac.uk>
2021-03-28 07:22:16 +0800

    Add dynamic error messages and redirecting in user sign-in flow

--- src/views/SignInView.js
+++ src/views/SignInView.js
@@ -1,17 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import $ from "jquery";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 function SignInView() {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  let history = useHistory();
+
+  const redirect = () => {
+    history.push("/");
+  };
+
 
   async function signInUser() {
     var username = $("#username")[0].value;
     var password = $("#password")[0].value;
 
     if (username == "" || password == "") {
-      Swal.fire({icon: 'error', title: 'Oops...', text: 'You haven\'t filled up your details!'});
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "You haven't filled up your details!",
+      });
       return;
     }
 
@@ -27,28 +39,50 @@ function SignInView() {
     if (response.ok) {
       var json_data = await response.json();
       if (json_data.success) {
-        $("#username")[0].value = "";
-        $("#password")[0].value = "";
-        await Swal.fire({icon: 'success', title: 'Welcome to WhereThe!', text: 'Happy surfing!'});
+        redirect();
+        await Swal.fire({
+          icon: "success",
+          title: "Welcome back to WhereThe!",
+          text: "Happy hunting!",
+        });
       } else {
-        await Swal.fire({icon: 'error', title: 'Oops...', text: 'Something went wrong!'});
+        await Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: json_data.error,
+        });
       }
     }
-
   }
 
-  return (<div>
-    <h1>Sign into your account</h1>
-    <div className="form-group">
-      <label htmlFor="username">Set a username: </label>
-      <input type="text" name="username" id="username" className="form-control" required/>
+  return (
+    <div>
+      <h1>Sign into your account</h1>
+      <div className="form-group">
+        <label htmlFor="username">Set a username: </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          className="form-control"
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Set a password: </label>
+        <input
+          type="text"
+          name="password"
+          id="password"
+          className="form-control"
+          required
+        />
+      </div>
+      <button onClick={signInUser} className="btn btn-primary">
+        Sign In
+      </button>
     </div>
-    <div className="form-group">
-      <label htmlFor="password">Set a password: </label>
-      <input type="text" name="password" id="password" className="form-control" required/>
-    </div>
-    <button onClick={signInUser} className="btn btn-primary">Sign In</button>
-  </div>);
+  );
 }
 
 export default SignInView;
