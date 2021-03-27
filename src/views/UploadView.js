397e4e1d6b34a3345b20853337dc4286d4c195d8
Vincent <kch19@ic.ac.uk>
2021-03-28 05:18:11 +0800

    added image take in for items upload page

--- src/views/UploadView.js
+++ src/views/UploadView.js
@@ -7,16 +7,29 @@ function UploadView() {
   const [contactEmail, setContactEmail] = useState("");
   const [contactNumber, setContactNumber] = useState("");
   const [date, setDate] = useState("");
+  const [image, setImage] = useState('');
+  const [imageShow, setImageShow] = useState('');
 
   async function uploadItem() {
     var name = $("#name")[0].value;
     var contactEmail = $("#contactEmail")[0].value;
     var contactNumber = $("#contactNumber")[0].value;
 
+    if (name == "" || contactEmail == "" || contactNumber == "") {
+      Swal.fire({icon: 'error', title: 'Oops...', text: 'You haven\'t filled up your details!'});
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", image[0]);
+
+    formData.append("contactEmail", contactEmail);
+    formData.append("contactNumber", contactNumber);
+    formData.append("name", name);
+
     let response = await fetch("http://127.0.0.1:5000/upload", {
       method: "POST",
-      body: JSON.stringify({"contactEmail": contactEmail, "contactNumber": contactNumber, "name": name}),
-      headers: new Headers({"content-type": "application/json"})
+      body: formData,
     });
 
     console.log(response);
@@ -28,6 +41,8 @@ function UploadView() {
         $("#name")[0].value = "";
         $("#contactEmail")[0].value = "";
         $("#contactNumber")[0].value = "";
+        setImageShow('');
+        setImage('');
         await Swal.fire({icon: 'success', title: 'Uploaded', text: 'Hope the item finds its owner!'});
       } else {
         await Swal.fire({icon: 'error', title: 'Oops...', text: 'Something went wrong!'});
@@ -36,21 +51,32 @@ function UploadView() {
 
   }
 
+  function upload(e) {
+        setImage(e.target.files);
+        if (e.target.files.length != 0) {
+          setImageShow(URL.createObjectURL(e.target.files[0]));
+        }
+    }
+
   return (<div>
     <h1>Upload your item</h1>
     <div className="form-group">
       <label htmlFor="name">What's the name of your item?</label>
-      <input type="text" name="name" id="name" className="form-control"/>
+      <input type="text" name="name" id="name" className="form-control" required/>
     </div>
     <div className="form-group">
       <label htmlFor="contactEmail">What's your contact email?</label>
-      <input type="text" name="contactEmail" id="contactEmail" className="form-control"/>
+      <input type="text" name="contactEmail" id="contactEmail" className="form-control" required/>
     </div>
     <div className="form-group">
       <label htmlFor="contactNumber">What's your contact number?</label>
-      <input type="text" name="contactNumber" id="contactNumber" className="form-control"/>
+      <input type="text" name="contactNumber" id="contactNumber" className="form-control" required/>
     </div>
-    <button onClick={uploadItem} className="btn btn-primary">FUCK SAKE</button>
+    <input type="file" name="file" accept="image/*" onChange={e => upload(e)}/>
+    <br />
+    <img src={imageShow} style={{ maxWidth: '100px', maxHeight: '100px' }} alt=""></img>
+    <br />
+    <button onClick={uploadItem} className="btn btn-primary">Upload</button>
   </div>);
 }
 
